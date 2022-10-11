7e00144f10d186da81d2a16ebce8069d576e76b9
Mehdi Saber <m1d3s@protonmail.com>
2022-10-11 14:35:54 +0200

    Basic VueJS Project scaffolding + WIP Docker implementation

---
+++ front/src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+    <header class="flex flex-col my-10 p-5">
+        <div class="flex flex-row items-center justify-evenly w-full">
+            <img
+                alt="Vue logo"
+                class="logo"
+                src="@/assets/logo.svg"
+                width="125"
+                height="125"
+            />
+            <img
+                alt="API Platform logo"
+                class="logo"
+                src="@/assets/api-platform-logo-round.svg"
+                width="125"
+                height="125"
+            />
+        </div>
+        <div class="space-y-5">
+            <HelloWorld msg="Et voilà !" />
+
+            <nav class="flex justify-center divide-x divide-gray-600">
+                <RouterLink to="/"><button class="mr-5 py-2 px-3 bg-cyan-600 text-white text-sm font-semibold rounded-full shadow-lg shadow-cyan-600/50 focus:outline-none transition-all hover:bg-cyan-500">Home</button></RouterLink>
+                <RouterLink to="/about"><button class="ml-5 py-2 px-3 bg-blue-600 text-white text-sm font-semibold rounded-full shadow-lg shadow-blue-600/50 focus:outline-none transition-all hover:bg-blue-500">About</button></RouterLink>
+            </nav>
+        </div>
+    </header>
+
+    <RouterView />
+</template>
+
+<style scoped>
+
+</style>
